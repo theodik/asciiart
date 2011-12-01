eb0c5e6405eb34301c084a5f584226334844fa5d
theodik <theodik@gmail.com>
2011-12-01 17:43:44 +0100

    File name...

--- src/asciiart/ASCIIart.java
+++ src/asciiart/ASCIIart.java
@@ -4,19 +4,44 @@
  */
 package asciiart;
 
+import java.io.File;
 import java.io.IOException;
+import java.util.Scanner;
 
 /**
  *
  * @author theodik
  */
 public class ASCIIart {
+    static String fontName, dir;
+    static int size, style;    
+    
+    public static void readParams(String[] args) {
+        if(args.length >= 3) {
+            fontName = args[0];
+            size = Integer.parseInt(args[1]);
+            style = Integer.parseInt(args[2]);
+            if(args.length > 3)
+                dir = args[3];
+        } else {
+            Scanner sc = new Scanner(System.in);
+            System.out.print("Font name: ");
+            fontName = sc.next();
+        }
+    }
 
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) throws IOException{
-        CharBuilder cb = new CharBuilder("Monospace", 12, 0);
-        cb.calcCharacters();
+    public static void main(String[] args) throws IOException {
+        readParams(args);
+
+        CharBuilder cb;
+        if (new File(fontName + "_" + size + "_" + style + ".txt").exists()) {
+            cb = new CharBuilder(dir + fontName + "_" + size + "_" + style + ".txt");
+        } else {
+            cb = new CharBuilder(dir + fontName, size, style);
+        }
+        cb.calcCharacters("");
     }
 }
--- src/asciiart/CharBuilder.java
+++ src/asciiart/CharBuilder.java
@@ -4,20 +4,25 @@
  */
 package asciiart;
 
+import com.sun.swing.internal.plaf.synth.resources.synth;
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.FontMetrics;
 import java.awt.Graphics;
 import java.awt.image.BufferedImage;
 import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map.Entry;
 import java.util.Scanner;
 import javax.imageio.ImageIO;
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 /**
  *
@@ -26,36 +31,36 @@ import javax.imageio.ImageIO;
 public class CharBuilder {
     public static final char REF_CHAR = 'H';
     public static final int  REF_WIDTH = 27;
-    public final ArrayList<CharPart> chars = new ArrayList<CharPart>();
+    //public final ArrayList<Integer> chars = new ArrayList<Integer>();
+    public final HashMap<Character, Integer> chars;
     public final Font font;
     
     public CharBuilder(String fontName, int size, int style) {
         font = new Font(fontName, style, size);
+        chars = new HashMap<Character, Integer>();
     }
     
     public CharBuilder(String fileName) throws FileNotFoundException {
         Scanner sc = new Scanner(new FileReader(fileName));
+        int mapSize = sc.nextInt();
+        chars = new HashMap<Character, Integer>(mapSize);
         String fontName = sc.nextLine();
         int style = sc.nextInt();
         int size = sc.nextInt();
         font = new Font(fontName, style, size);
         while (sc.hasNextLine()) {
-            CharPart cp = new CharPart(sc.next().charAt(0), sc.nextInt(), sc.nextInt(), sc.nextInt(), sc.nextInt());
-            chars.add(cp);
+            chars.put(sc.next().charAt(0), sc.nextInt());
         }
     }
     
-    public void calcCharacters() throws IOException{
+    public void calcCharacters(String dir) throws IOException {
         final BufferedImage img = new BufferedImage(font.getSize()*2, font.getSize()*2, BufferedImage.TYPE_BYTE_GRAY);
         final Graphics g = img.getGraphics();
         FontMetrics fm = g.getFontMetrics();
         g.setFont(font);
         
-        BufferedWriter wr = new BufferedWriter(new FileWriter(this.font.getName()+".txt"));
-        wr.write(font.getName() + "\n");
-        wr.write(font.getStyle() + "\n");
-        wr.write(font.getSize() + "\n");
-        
+        StringBuilder data = new StringBuilder();
+        int count = 0;
         for (char i = 0; i < Character.MAX_VALUE; i++) {
             if (font.canDisplay(i) && fm.charWidth(i) == fm.charWidth(REF_CHAR)) {
                 g.setColor(Color.white);
@@ -64,16 +69,36 @@ public class CharBuilder {
                 g.drawString(Character.toString(i), 0, fm.getMaxAscent());
                 img.flush();
                 //saveImgToFile("chars/"+(int)i+".png", img);
-                CharPart cp = new CharPart((char)i, img, 0,0, fm.charWidth(i), fm.getHeight());
-                wr.write(cp.toString() + "\n");
+                int avgColor = avgColor(img, 0,0, fm.charWidth(i), fm.getHeight());
+                chars.put((Character)i,avgColor & 0xFF);
+                data.append((char)i).append(" ").append(avgColor & 0xFF).append("\n");
+                count++;
             }
         }
+        BufferedWriter wr = new BufferedWriter(
+                new FileWriter(
+                dir + this.font.getName() + "_" + this.font.getSize() + "_" + this.font.getStyle() + ".txt"
+                ));
+        wr.write(count + "\n");
+        wr.write(font.getName() + "\n");
+        wr.write(font.getStyle() + "\n");
+        wr.write(font.getSize() + "\n");
+        wr.append(data);
         wr.close();
     }
     
-    public char getChar(CharPart cp) {
-        
-        return '0';
+    public char getChar(int avgColor) {
+        int ac = avgColor & 0xFF;
+        int min = 0;
+        char ret = '\0';
+        for (Entry<Character, Integer> item : chars.entrySet()) {
+            int amin = Math.abs(item.getValue() - ac);
+            if (amin < min) {
+                min = amin;
+                ret = item.getKey();
+            }
+        }
+        return ret;
     }
     
     public static void saveImgToFile(String fileName, BufferedImage img) {
@@ -84,5 +109,21 @@ public class CharBuilder {
         } catch (Exception e) {
             System.out.println("e = " + e.getLocalizedMessage());
         }
+    }
+    
+    public static int avgColor(BufferedImage img, int x, int y, int w, int h) {
+        int R = 0, G = 0, B = 0;
+        for (int i = x; i < w; i++) {
+            for (int j = y; j < h; j++) {
+                int P = img.getRGB(i,j);
+                R += P>>16 & 0xFF;
+                G += P>>8 & 0xFF;
+                B += P & 0xFF;
+            }
+        }
+        R /= (w-x)*(h-y);
+        G /= (w-x)*(h-y);
+        B /= (w-x)*(h-y);
+        return (R<<16)+(G<<8)+B;
     }
 }
