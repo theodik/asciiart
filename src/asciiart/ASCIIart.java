ced51f24612b57181228191b6b6ec1890ab2f806
theodik <theodik@gmail.com>
2011-12-03 19:48:20 +0100

    Func ver1.

--- src/asciiart/ASCIIart.java
+++ src/asciiart/ASCIIart.java
@@ -1,47 +1,120 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package asciiart;
 
+import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.Scanner;
+import javax.imageio.ImageIO;
 
 /**
  *
  * @author theodik
  */
 public class ASCIIart {
-    static String fontName, dir;
-    static int size, style;    
-    
-    public static void readParams(String[] args) {
-        if(args.length >= 3) {
-            fontName = args[0];
-            size = Integer.parseInt(args[1]);
-            style = Integer.parseInt(args[2]);
-            if(args.length > 3)
-                dir = args[3];
-        } else {
-            Scanner sc = new Scanner(System.in);
-            System.out.print("Font name: ");
-            fontName = sc.next();
-        }
-    }
+    static CharBuilder cb;
+    static String fontName, ifile, ofile;
+    static int size, style;
 
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         readParams(args);
-
-        CharBuilder cb;
+        
         if (new File(fontName + "_" + size + "_" + style + ".txt").exists()) {
-            cb = new CharBuilder(dir + fontName + "_" + size + "_" + style + ".txt");
+            System.out.println("Načítám data ze souboru: '"+fontName + "_" + size + "_" + style + ".txt'");
+            try {
+                cb = new CharBuilder(fontName + "_" + size + "_" + style + ".txt");
+            } catch(FileNotFoundException e) {
+                System.out.println("Soubor nenalezen: "+e);
+                return;
+            }
         } else {
-            cb = new CharBuilder(dir + fontName, size, style);
+            cb = new CharBuilder(fontName, size, style);
+            System.out.println("Vytvářím soubor se znaky: "+fontName + "_" + size + "_" + style + ".txt");
+            try {
+                cb.calcCharacters();
+            } catch(IOException e) {
+                System.out.println("Nelze zapsat soubor se znaky: "+e);
+                (new File(fontName + "_" + size + "_" + style + ".txt")).delete();
+                return;
+            }
         }
-        cb.calcCharacters("");
+        BufferedImage img;
+        FileWriter fw;
+        try{
+            FileInputStream fs = new FileInputStream(ifile);
+            BufferedImage orig = ImageIO.read(fs);
+            img = new BufferedImage(orig.getWidth(), orig.getHeight(), BufferedImage.TYPE_BYTE_GRAY);
+            img.getGraphics().drawImage(orig, 0, 0, null);
+            //CharBuilder.saveImgToFile("input-test.jpg", img);
+            fs.close();
+        } catch(IOException e){
+            System.out.println("Nemohl jsem načíst vstupní obrázek: "+e);
+            return;
+        }
+        try {
+            fw = new FileWriter(ofile);
+            System.out.println("Vytvářím obrázek...");
+            createArt(img, fw);
+            fw.close();
+            System.out.println("Hotovo.");
+        } catch(IOException e) {
+            System.out.println(e);
+            return;
+        }
     }
+    
+    /**
+     * Zpracuje argumenty, pokud nejsou zeptá se.
+     * @param args the command line arguments
+     */
+    public static void readParams(String[] args) {
+        if(args.length >= 3) {
+            ifile = args[0];
+            ofile = args[1];
+            fontName = args[2];
+            size = Integer.parseInt(args[3]);
+            style = Integer.parseInt(args[4]);
+        } else {
+            Scanner sc = new Scanner(System.in);
+            System.out.print("Input image: ");
+            ifile = sc.next();
+            System.out.print("\nOutput file: ");
+            ofile = sc.next();
+            System.out.print("Font name: ");
+            fontName = sc.next();
+            System.out.print("\nFont size: ");
+            size = sc.nextInt();
+            System.out.print("\nFont style (0: none, 1: bold, 2: italic): ");
+            style = sc.nextInt();
+        }
+    }
+    
+    public static void createArt(BufferedImage img, FileWriter fw) throws IOException{
+        int w,h,dx,dy;
+        w = (int)Math.floor(img.getWidth() / CharBuilder.REF_WIDTH);
+        dx = CharBuilder.REF_WIDTH;
+        h = (int)Math.floor(img.getHeight() / CharBuilder.REF_HEIGHT);
+        dy = CharBuilder.REF_HEIGHT;
+        int charCount=0, lineCount=0;
+        for (int j = 0; j < h; j++) {
+            for (int i = 0; i < w; i++) {
+                int avgColor = CharBuilder.avgColor8bit(img, i*dx, j*dy, (i+1)*dx, (j+1)*dy);
+                char aChar = cb.getChar(avgColor);
+                fw.write(Character.toString(aChar));
+                charCount++;
+            }
+            fw.write("\n");
+            lineCount++;
+        }
+        System.out.println("lineCount = " + lineCount);
+        System.out.println("charCount = " + charCount);
+    }
+
 }
--- src/asciiart/CharBuilder.java
+++ src/asciiart/CharBuilder.java
@@ -4,92 +4,132 @@
  */
 package asciiart;
 
-import com.sun.swing.internal.plaf.synth.resources.synth;
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.FontMetrics;
 import java.awt.Graphics;
 import java.awt.image.BufferedImage;
 import java.io.BufferedWriter;
-import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map.Entry;
 import java.util.Scanner;
 import javax.imageio.ImageIO;
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 /**
- *
+ * Vytvoří a udržuje seznam písmen a jejich průměrných barev.
  * @author theodik
  */
 public class CharBuilder {
+    /**
+     * Referenční znak
+     */
     public static final char REF_CHAR = 'H';
-    public static final int  REF_WIDTH = 27;
-    //public final ArrayList<Integer> chars = new ArrayList<Integer>();
-    public final HashMap<Character, Integer> chars;
-    public final Font font;
+    public static final int REF_VAR = 10;
+    /**
+     * Šířka referenčního znaku.
+     */
+    public static int  REF_WIDTH = 27;
+    /**
+     * Výška referenčního znaku
+     */
+    public static int REF_HEIGHT;
+    private final HashMap<Character, Integer> chars;
+    private final Font font;
     
+    /**
+     * Vytvoří prázdný seznam písmen.
+     * Naplní se pomocí {@link #calcCharacters(java.lang.String) calcCharacters}
+     * @param fontName Jméno fontu
+     * @param size Velikost fontu
+     * @param style Styl fontu (0: none, 1: bold, 2: italic)
+     */
     public CharBuilder(String fontName, int size, int style) {
         font = new Font(fontName, style, size);
         chars = new HashMap<Character, Integer>();
     }
     
+    /**
+     * Vytvoří a naplní znaky ze souboru.
+     * @param fileName Soubor se znaky
+     * @throws FileNotFoundException 
+     */
     public CharBuilder(String fileName) throws FileNotFoundException {
         Scanner sc = new Scanner(new FileReader(fileName));
+        //sc.useDelimiter("\n *");
         int mapSize = sc.nextInt();
-        chars = new HashMap<Character, Integer>(mapSize);
+        sc.nextLine();
         String fontName = sc.nextLine();
         int style = sc.nextInt();
         int size = sc.nextInt();
+        REF_HEIGHT = sc.nextInt();
+        REF_WIDTH = sc.nextInt();
+        
+        chars = new HashMap<Character, Integer>(mapSize);
         font = new Font(fontName, style, size);
-        while (sc.hasNextLine()) {
-            chars.put(sc.next().charAt(0), sc.nextInt());
+        while (sc.hasNext()) {
+            chars.put((char)sc.nextInt(), sc.nextInt());
         }
     }
     
-    public void calcCharacters(String dir) throws IOException {
+    /**
+     * Vytvoří seznam s písmeny, spočítá jejich průměrnou barvu a uloží do souboru
+     * v aktuální složce ve formátu <code>fontName_fontSize_fontStyle.txt</code>.
+     * @throws IOException 
+     */
+    public void calcCharacters() throws IOException {
         final BufferedImage img = new BufferedImage(font.getSize()*2, font.getSize()*2, BufferedImage.TYPE_BYTE_GRAY);
         final Graphics g = img.getGraphics();
         FontMetrics fm = g.getFontMetrics();
         g.setFont(font);
         
+        REF_WIDTH = fm.charWidth(REF_CHAR);
+        REF_HEIGHT = fm.getHeight();
+        
         StringBuilder data = new StringBuilder();
         int count = 0;
         for (char i = 0; i < Character.MAX_VALUE; i++) {
-            if (font.canDisplay(i) && fm.charWidth(i) == fm.charWidth(REF_CHAR)) {
+            int charWidth = fm.charWidth(i);
+            //if (font.canDisplay(i)  && (charWidth > REF_WIDTH && charWidth < REF_WIDTH+REF_VAR)) {
+            if(font.canDisplay(i) && charWidth > 0) {
                 g.setColor(Color.white);
                 g.fillRect(0, 0, img.getWidth(), img.getHeight());
                 g.setColor(Color.black);
                 g.drawString(Character.toString(i), 0, fm.getMaxAscent());
                 img.flush();
                 //saveImgToFile("chars/"+(int)i+".png", img);
-                int avgColor = avgColor(img, 0,0, fm.charWidth(i), fm.getHeight());
-                chars.put((Character)i,avgColor & 0xFF);
-                data.append((char)i).append(" ").append(avgColor & 0xFF).append("\n");
+                int avgColor = avgColor8bit(img, 0,0, fm.charWidth(i), fm.getHeight());
+                chars.put((Character)i,avgColor);
+                data.append((int)i).append(" ").append(avgColor & 0xFF).append("\n");
                 count++;
             }
         }
         BufferedWriter wr = new BufferedWriter(
                 new FileWriter(
-                dir + this.font.getName() + "_" + this.font.getSize() + "_" + this.font.getStyle() + ".txt"
+                    this.font.getName() + "_" + this.font.getSize() + "_" + this.font.getStyle() + ".txt"
                 ));
         wr.write(count + "\n");
         wr.write(font.getName() + "\n");
         wr.write(font.getStyle() + "\n");
         wr.write(font.getSize() + "\n");
+        wr.write(REF_HEIGHT + "\n");
+        wr.write(REF_WIDTH + "\n");
         wr.append(data);
         wr.close();
     }
     
+    /**
+     * Vrací podle barvy nejvhodnější znak.
+     * @param avgColor Barva šedi podle které se vybere znak
+     * @return Nejvhodnější znak.
+     */
     public char getChar(int avgColor) {
         int ac = avgColor & 0xFF;
-        int min = 0;
+        int min = Integer.MAX_VALUE;
         char ret = '\0';
         for (Entry<Character, Integer> item : chars.entrySet()) {
             int amin = Math.abs(item.getValue() - ac);
@@ -125,5 +165,17 @@ public class CharBuilder {
         G /= (w-x)*(h-y);
         B /= (w-x)*(h-y);
         return (R<<16)+(G<<8)+B;
+    }
+    
+    public static int avgColor8bit(BufferedImage img, int x, int y, int w, int h) {
+        int B = 0;
+        for (int i = x; i < w; i++) {
+            for (int j = y; j < h; j++) {
+                int P = img.getRGB(i,j);
+                B += P & 0xFF;
+            }
+        }
+        B /= (w-x)*(h-y);
+        return B;
     }
 }
