bc3778ad3d8e275f438e2e1fca5a478d7e1e9fd6
theodik <theodik@gmail.com>
2011-12-05 22:36:12 +0100

    Nějaký změny ...

--- src/asciiart/ASCIIart.java
+++ src/asciiart/ASCIIart.java
@@ -4,10 +4,8 @@ import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.io.InputStream;
 import java.util.Scanner;
 import javax.imageio.ImageIO;
 
